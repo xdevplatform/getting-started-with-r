9da8c154e1d1b4aef66657d53f300aae6feb9cf4
Jessica Garson <jessica.garson@gmail.com>
2019-05-23 18:13:52 -0400

    Create synths.r

---
+++ synths.r
@@ -0,0 +1,11 @@
+install.packages("rtweet")
+library(rtweet)
+
+synths <- search_tweets(
+  "#synths", n = 18000, include_rts = FALSE
+)
+
+View(synths)
+
+write_as_csv(synths, file_name = "synths.csv", prepend_ids = TRUE, na = "", 
+             fileEncoding = "UTF-8")
